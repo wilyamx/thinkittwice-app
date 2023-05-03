678ea61b62757f734ab0aa4b55351f7129367f0f
William Reña <william.rena@accenture.com>
2023-05-03 08:51:49 +0800

    Add new project for swift ui

---
+++ DailyTraining-SwiftUI/DailyTraining-SwiftUI/DailyTraining_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTraining_SwiftUIApp.swift
+//  DailyTraining-SwiftUI
+//
+//  Created by William Rena on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyTraining_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
