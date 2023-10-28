99e2b8959a1be08549b28e1e3b2a848dea6e9804
William Rena <wilyamx@gmail.com>
2023-10-28 14:38:40 +0800

    Refactor view using @ViewBuilder

--- DailyTraining-SwiftUI/DailyTraining-SwiftUI/Modules/LoginView/LoginView.swift
+++ DailyTraining-SwiftUI/DailyTraining-SwiftUI/Modules/LoginView/LoginView.swift
@@ -40,159 +40,28 @@ struct LoginView: View {
                         }
                         .padding()
                         
+                        // Email and Password Entry
                         VStack(alignment: .leading, spacing: 10) {
-                            ZStack(alignment: .trailing) {
-                                TextField("Email", text: $viewModel.username)
-                                    .font(.system(size: 17, weight: .thin))
-                                    .foregroundColor(.primary)
-                                    .frame(height: 44)
-                                    .padding(.horizontal, 12)
-                                    .background(ColorNames.accentColor.colorValue.opacity(0.25))
-                                    .cornerRadius(4.0)
-                                    .focused($focusField, equals: .username)
-                                    .disableAutocorrection(true)
-                                    .autocapitalization(.none)
-                                    .keyboardType(.emailAddress)
-                                
-                                Button {
-                                    viewModel.username = ""
-                                } label: {
-                                    Image(systemName: "xmark")
-                                }
-                                .padding(.trailing)
-                            }
-                            
-                            ZStack(alignment: .trailing) {
-                                if viewModel.isSecured {
-                                    SecureField("Password", text: $viewModel.password)
-                                        .font(.system(size: 17, weight: .thin))
-                                        .foregroundColor(.primary)
-                                        .frame(height: 44)
-                                        .padding(.horizontal, 12)
-                                        .background(ColorNames.accentColor.colorValue.opacity(0.25))
-                                        .cornerRadius(4.0)
-                                        .focused($focusField, equals: .password)
-                                        .disableAutocorrection(true)
-                                        .autocapitalization(.none)
-                                        .onSubmit {
-                                            viewModel.login()
-                                        }
-                                }
-                                else {
-                                    TextField("Password", text: $viewModel.password)
-                                        .font(.system(size: 17, weight: .thin))
-                                        .foregroundColor(.primary)
-                                        .frame(height: 44)
-                                        .padding(.horizontal, 12)
-                                        .background(ColorNames.accentColor.colorValue.opacity(0.25))
-                                        .cornerRadius(4.0)
-                                        .focused($focusField, equals: .password)
-                                        .disableAutocorrection(true)
-                                        .autocapitalization(.none)
-                                        .onSubmit {
-                                            viewModel.login()
-                                        }
-                                }
-                                
-                                Button {
-                                    viewModel.isSecured.toggle()
-                                } label: {
-                                    Image(systemName: viewModel.isSecured ? "eye" : "eye.slash")
-                                }
-                                .padding(.trailing)
-                            }
-                            
-                            Button(action: {
-                                viewModel.login()
-                            },
-                                   label: {
-                                Text("Login")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.white)
-                                    .frame(maxWidth: .infinity)
-                                    .frame(height: 40)
-                                    .background(.purple)
-                                    .clipShape(Capsule())
-                            })
-                            .padding(.vertical)
-                            .alert(isPresented: $viewModel.showingAlert) {
-                                Alert(title: Text("Invalid credentials"),
-                                      dismissButton: .default(Text("Got it!")) {
-                                    viewModel.password = ""
-                                    focusField = .password
-                                    }
-                                )
-                            }
+                            emailInputView
+                            passwordInputView
+                            loginButton
                         }
                         .padding(.horizontal)
                         
-                        HStack {
-                            Rectangle()
-                                .fill(.black.opacity(0.3))
-                                .frame(height: 1)
-                            
-                            Text("OR")
-                                .fontWeight(.bold)
-                                .foregroundColor(.black.opacity(0.3))
-                            Rectangle()
-                                .fill(.black.opacity(0.3))
-                                .frame(height: 1)
-                        }
-                        .padding(.horizontal)
+                        // Divider
+                        divider
                         
-                        HStack {
-                            Link(destination: URL(string: "https://www.facebook.com/")!,
-                                 label: {
-                                HStack(spacing: 5) {
-                                    Image(systemName: "square.and.arrow.up")
-                                        .foregroundColor(.white)
-                                    Text("Facebook")
-                                        .fontWeight(.bold)
-                                        .foregroundColor(.white)
-                                }
-                                .padding(.vertical, 10)
-                                .frame(width: (UIScreen.main.bounds.width - 80) / 2)
-                                .background(.blue)
-                                .clipShape(Capsule())
-                            })
-                            .padding(.bottom)
-
-                            Link(destination: URL(string: "https://www.google.com/")!,
-                                 label: {
-                                HStack(spacing: 5) {
-                                    Image(systemName: "globe")
-                                        .foregroundColor(.white)
-                                    Text("Google")
-                                        .fontWeight(.bold)
-                                        .foregroundColor(.white)
-                                }
-                                .padding(.vertical, 10)
-                                .frame(width: (UIScreen.main.bounds.width - 80) / 2)
-                                .background(.red)
-                                .clipShape(Capsule())
-                            })
-                            .padding(.bottom)
-                            
-                        }
-                        .padding(.horizontal)
-                        
+                        // Facebook and Google
+                        buttonLink
                     }
-                    
                     .background(.white)
                     .cornerRadius(20)
                     .padding()
                     
                     Spacer()
                     
-                    HStack {
-                        Text("New around here?")
-                        Text("Sign in")
-                            .foregroundColor(Color.red)
-                            .onTapGesture {
-                                isPresentedRegistration = true
-                            }
-                    }
-                    
+                    // Signin Link
+                    signinLink
                 }
                 
             }
@@ -210,6 +79,166 @@ struct LoginView: View {
             .fullScreenCover(isPresented: $isPresentedRegistration) {
                 RegistrationView()
             }
+        }
+    }
+    
+    @ViewBuilder
+    private var emailInputView: some View {
+        ZStack(alignment: .trailing) {
+            TextField("Email", text: $viewModel.username)
+                .font(.system(size: 17, weight: .thin))
+                .foregroundColor(.primary)
+                .frame(height: 44)
+                .padding(.horizontal, 12)
+                .background(ColorNames.accentColor.colorValue.opacity(0.25))
+                .cornerRadius(4.0)
+                .focused($focusField, equals: .username)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+            
+            Button {
+                viewModel.username = ""
+            } label: {
+                Image(systemName: "xmark")
+            }
+            .padding(.trailing)
+        }
+    }
+    
+    @ViewBuilder
+    private var passwordInputView: some View {
+        ZStack(alignment: .trailing) {
+            if viewModel.isSecured {
+                SecureField("Password", text: $viewModel.password)
+                    .font(.system(size: 17, weight: .thin))
+                    .foregroundColor(.primary)
+                    .frame(height: 44)
+                    .padding(.horizontal, 12)
+                    .background(ColorNames.accentColor.colorValue.opacity(0.25))
+                    .cornerRadius(4.0)
+                    .focused($focusField, equals: .password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .onSubmit {
+                        viewModel.login()
+                    }
+            }
+            else {
+                TextField("Password", text: $viewModel.password)
+                    .font(.system(size: 17, weight: .thin))
+                    .foregroundColor(.primary)
+                    .frame(height: 44)
+                    .padding(.horizontal, 12)
+                    .background(ColorNames.accentColor.colorValue.opacity(0.25))
+                    .cornerRadius(4.0)
+                    .focused($focusField, equals: .password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .onSubmit {
+                        viewModel.login()
+                    }
+            }
+            
+            Button {
+                viewModel.isSecured.toggle()
+            } label: {
+                Image(systemName: viewModel.isSecured ? "eye" : "eye.slash")
+            }
+            .padding(.trailing)
+        }
+    }
+    
+    @ViewBuilder
+    private var loginButton: some View {
+        Button(action: {
+            viewModel.login()
+        },
+               label: {
+            Text("Login")
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .frame(height: 40)
+                .background(.purple)
+                .clipShape(Capsule())
+        })
+        .padding(.vertical)
+        .alert(isPresented: $viewModel.showingAlert) {
+            Alert(title: Text("Invalid credentials"),
+                  dismissButton: .default(Text("Got it!")) {
+                viewModel.password = ""
+                focusField = .password
+                }
+            )
+        }
+    }
+    
+    @ViewBuilder
+    private var divider: some View {
+        HStack {
+            Rectangle()
+                .fill(.black.opacity(0.3))
+                .frame(height: 1)
+            
+            Text("OR")
+                .fontWeight(.bold)
+                .foregroundColor(.black.opacity(0.3))
+            Rectangle()
+                .fill(.black.opacity(0.3))
+                .frame(height: 1)
+        }
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    private var buttonLink: some View {
+        HStack {
+            Link(destination: URL(string: "https://www.facebook.com/")!,
+                 label: {
+                HStack(spacing: 5) {
+                    Image(systemName: "square.and.arrow.up")
+                        .foregroundColor(.white)
+                    Text("Facebook")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical, 10)
+                .frame(width: (UIScreen.main.bounds.width - 80) / 2)
+                .background(.blue)
+                .clipShape(Capsule())
+            })
+            .padding(.bottom)
+
+            Link(destination: URL(string: "https://www.google.com/")!,
+                 label: {
+                HStack(spacing: 5) {
+                    Image(systemName: "globe")
+                        .foregroundColor(.white)
+                    Text("Google")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical, 10)
+                .frame(width: (UIScreen.main.bounds.width - 80) / 2)
+                .background(.red)
+                .clipShape(Capsule())
+            })
+            .padding(.bottom)
+            
+        }
+        .padding(.horizontal)
+    }
+    
+    @ViewBuilder
+    private var signinLink: some View {
+        HStack {
+            Text("New around here?")
+            Text("Sign in")
+                .foregroundColor(Color.red)
+                .onTapGesture {
+                    isPresentedRegistration = true
+                }
         }
     }
 }
